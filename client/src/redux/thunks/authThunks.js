8d9a394557db498fc5a484df9bb97f9275296719
Bartek.B <bartek.bugala@gmail.com>
2020-02-10 23:07:46 +0100

    refactor authThunks.js

--- client/src/redux/thunks/authThunks.js
+++ client/src/redux/thunks/authThunks.js
@@ -10,34 +10,25 @@ import {
   errorRequest
 } from '../actions/requestsActions';
 
-/* export const registerUserRequest = (userData, history) => {
+export const registerUser = (userData, history) => {
   return async dispatch => {
     dispatch(startRequest());
     try {
       await axios.post(`${API_URL}/register`, userData);
-      res => history.push('/login');
+      history.push('/login');
       dispatch(endRequest());
-    } catch (e) {
-      dispatch(errorRequest(JSON.stringify(e)));
+    } catch (err) {
+      dispatch(errorRequest(err.response.data));
+      dispatch(getErrors(err.response.data));
     }
   };
-}; */
-
-export const registerUser = (userData, history) => dispatch => {
-  axios
-    .post('http://localhost:8000/api/register', userData)
-    .then(res => history.push('/login')) // re-direct to login on successful register
-    .catch(err => dispatch(getErrors(err.response.data)));
 };
 
-// Login - get user token
-export const loginUser = userData => dispatch => {
-  axios
-    .post('http://localhost:8000/api/login', userData)
-    .then(res => {
-      console.log('login ok', res);
-      // Save to localStorage
-      // Set token to localStorage
+export const loginUser = userData => {
+  return async dispatch => {
+    dispatch(startRequest());
+    try {
+      let res = await axios.post(`${API_URL}/login`, userData);
       const { token } = res.data;
       localStorage.setItem('jwtToken', token);
       // Set token to Auth header
@@ -46,16 +37,21 @@ export const loginUser = userData => dispatch => {
       const decoded = jwt_decode(token);
       // Set current user
       dispatch(setCurrentUser(decoded));
-    })
-    .catch(err => dispatch(getErrors(err.response.data)));
-};
-const USER_LOADING = 'USER_LOADING';
-// User loading
-export const setUserLoading = () => {
-  return {
-    type: USER_LOADING
+      dispatch(endRequest());
+    } catch (err) {
+      dispatch(errorRequest(err.response.data));
+      dispatch(getErrors(err.response.data));
+    }
   };
 };
+
+// User loading
+export const setUserLoading = () => {
+  return dispatch => {
+    dispatch(userLoading());
+  };
+};
+
 // Log user out
 export const logoutUser = () => dispatch => {
   // Remove token from local storage
