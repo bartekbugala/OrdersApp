5868deb2f539df84f295828a68ffb191393a468f
Bartek.B <bartek.bugala@gmail.com>
2020-02-14 21:00:29 +0100

    add form initValues to redux initial state add: client/src/redux/reducers/initValues.js edit: client/src/redux/reducers/productions.js

---
+++ client/src/redux/reducers/initValues.js
@@ -0,0 +1,58 @@
+export const emptyProduction = {
+  orderNumber: '',
+  clientName: '',
+  downpayment: '',
+  productionTerm: '',
+  finalPayment: false,
+  type: '',
+  colorOutside: '',
+  colorInside: '',
+  core: '',
+  thickness: '',
+  finished: false,
+  productionDate: '',
+  canceled: false,
+  transported: false,
+  m2: '',
+  csa: ''
+};
+
+export const panelThicknesses = [
+  20,
+  25,
+  30,
+  35,
+  40,
+  45,
+  50,
+  55,
+  60,
+  80,
+  100,
+  120,
+  150,
+  200,
+  250
+];
+
+export const panelTypes = ['D', 'S', 'SP-L', 'SP', 'T35'];
+export const panelCores = ['St', 'Wm', 'XPS', 'PUR'];
+export const cSAgents = ['BB', 'B', 'S'];
+const otherCovers = ['PERF', 'NR', 'STAND'];
+export const insideColors = [
+  'BIALY',
+  '9002',
+  '9010',
+  '9016',
+  '9006',
+  ...otherCovers
+];
+export const outsideColors = [
+  ...insideColors,
+  '7035',
+  '7024',
+  '1015',
+  '8017',
+  '3011',
+  ...otherCovers
+];
